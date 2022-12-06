4eff16c31cf28afcdc45cc292bca97f978a42d79
林家暄 <11033102@gm.chihlee.edu.tw>
2022-12-06 19:24:17 +0800

    magic square

---
+++ magic square/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace magic_square
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請輸入欲產生的n*n陣列：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(17, 79);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(430, 349);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "產生";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 454);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "magic square";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ magic square/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace magic_square
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = ""; //結果輸出
+            double num = Convert.ToInt32(textBox1.Text);
+            int result = Convert.ToInt32(Math.Floor(num / 2));//設定數字1的位子
+            int[,] array = new int[Convert.ToInt32(num), Convert.ToInt32(num)]; //呼叫矩陣
+
+            //所有矩陣的位子內容皆設為0
+            for (int i = 0; i < num; i++)
+                for (int j = 0; j < num; j++)
+                    array[i, j] = 0;
+
+            int temp_r = 0;
+            int temp_c = result; //數字1 列的位置
+            int temp = (int)num; //轉為數字
+            array[0, result] = 1; //設定1的位子
+            int a = 1; //判斷是否需要換行    
+
+            //因已設定1的位子 故從2開始判斷
+            for (int i = 2; i < (num * num) + 1; i++)
+            {
+                if (temp_r == 0) //如行在0時執行
+                {
+                    if (temp_c == 0) //如行列都在0 則 行+1
+                        temp_r += 1;
+                    else  //如行在0 列不在0則代表需要去左行最底下
+                    {
+                        temp_r = temp - 1; //依據輸入值-1判斷總高度 往下
+                        temp_c -= 1; //去左行
+                    }
+                }
+                else if (temp_c == 0)//如列在0 列不在0 則代表需要最右邊的右上位置
+                {
+                    temp_r -= 1; //往上
+                    temp_c = temp - 1;//依據輸入值-1判斷總寬度  往右
+                }
+                else if (array[temp_r - 1, temp_c - 1] != 0) //若已有值存入 則往下一行書寫
+                    temp_r += 1;
+                else //如這邊沒有值 也行列皆不為0 則往左上
+                {
+                    temp_r -= 1;
+                    temp_c -= 1;
+                }
+                array[temp_r, temp_c] = i; //寫入值
+            }
+            foreach (int i in array) //將矩陣存入的內容輸出
+            {
+                str += string.Format(" {0:00} ", i);
+                if (a % num == 0)
+                    str += string.Format("\r\n");
+                a++;
+            }
+            textBox2.Text = str;
+        }
+    }
+}
